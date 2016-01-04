cac64df1a6fbba7c41140d1df937b587cbd57cd1
simplon-mars-2-Abdel-Malik <abdelmalik.bouali@yahoo.com>
2016-01-04 09:44:42 +0100

    modification des erreurs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-$("#browser").text(window.navigator.userAgent);
-$(window).on('resize,load', function(event);
-{ $("width").text(window.innerWidth);
-  $("height").text(window.innerHeight);
+$("#browser").text(navigator.userAgent); // c'est navigator.userAgent et non pas window.navigator
+$(window).on('resize', 'load', function(event) // il faut séparer 'resize' et 'load' par une virgule
+{ 
+  $(".width").text(window.innerWidth); // width est une class dans le html, il faut donc mettre le point
+  $(".height").text(window.innerHeight); // idem
+}); // erreur oublie d'accolade et parenthèse
 });
